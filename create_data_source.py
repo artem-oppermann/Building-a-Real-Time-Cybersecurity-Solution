61df20d61480101a9b5b7c21e641d73d33bb6e71
Artem Oppermann <artem.oppermann@uni-oldenburg.de>
2023-12-20 13:21:22 +0100

    Create create_data_source.py

---
+++ create_data_source.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+#conn = psycopg2.connect(database="dev", user="root", password="", host="localhost", port="4566") # Connect to RisingWave.
+conn = psycopg2.connect(host="localhost", port=4566, user="root", dbname="dev")
+
+
+conn.autocommit = True # Set queries to be automatically committed.
+
+with conn.cursor() as cur:
+    cur.execute("""
+CREATE SOURCE IF NOT EXISTS log_data2 (
+ timestamp varchar,
+ ip_address varchar,
+ user varchar,
+ action varchar,
+ resource varchar,
+ status_code varchar
+ )
+WITH (
+ connector='kafka',
+ topic='log_data',
+ properties.bootstrap.server='localhost:9093',
+ scan.startup.mode='earliest'
+ ) FORMAT PLAIN ENCODE JSON;""") # Execute the query.
+
+conn.close() # Close the connection.
