ec0df1e70bb96f8e83adb3edfd7498df6c072339
Artem Oppermann <artem.oppermann@uni-oldenburg.de>
2023-12-20 13:22:54 +0100

    Create sink.py

---
+++ sink.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+conn = psycopg2.connect(host="localhost", port=4566, user="root", dbname="dev")
+
+conn.autocommit = True 
+
+with conn.cursor() as cur:
+    cur.execute("""
+        CREATE SINK sink_to_kafka FROM anomaly_detection_by_error_status
+        WITH (
+        connector='kafka',
+        properties.bootstrap.server='localhost:9093',
+        topic='anomalies'
+        ) FORMAT PLAIN ENCODE JSON (
+        force_append_only='true',
+        );""") # Execute the query.
+            
+conn.close() 
