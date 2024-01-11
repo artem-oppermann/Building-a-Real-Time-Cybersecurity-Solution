6aea2804fffb62e7a7b4b9cd18d5e42afadc69a1
Artem Oppermann <artem.oppermann@uni-oldenburg.de>
2024-01-11 08:17:35 +0100

    Update show_result.py

--- show_result.py
+++ show_result.py
@@ -7,12 +7,12 @@ conn.autocommit = True
 
 with conn.cursor() as cur:
     cur.execute("""
- SELECT * FROM anomaly_detection_by_error_status; """) # Execute the query.
+ SELECT * FROM anomaly_detection_by_error_status; """) #Execute the query.
     
-    # Fetch all rows from the executed query.
+    #Fetch all rows from the executed query.
     rows = cur.fetchall()
     
-    # Iterate through the rows and print each one.
+    #Iterate through the rows and print each one.
     for row in rows:
         print(row)
 
