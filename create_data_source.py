5a83001ab83d7f2bbdcb098c499ef95cd27f9629
Artem Oppermann <artem.oppermann@uni-oldenburg.de>
2024-01-11 08:15:38 +0100

    Update create_data_source.py

--- create_data_source.py
+++ create_data_source.py
@@ -3,7 +3,7 @@ import psycopg2
 conn = psycopg2.connect(host="localhost", port=4566, user="root", dbname="dev")
 
 
-conn.autocommit = True # Set queries to be automatically committed.
+conn.autocommit = True#Set queries to be automatically committed
 
 with conn.cursor() as cur:
     cur.execute("""
@@ -20,6 +20,6 @@ WITH (
  topic='log_data',
  properties.bootstrap.server='localhost:9093',
  scan.startup.mode='earliest'
- ) FORMAT PLAIN ENCODE JSON;""") # Execute the query.
+ ) FORMAT PLAIN ENCODE JSON;""") # Execute the query
 
-conn.close() # Close the connection.
+conn.close()#Close the connection
